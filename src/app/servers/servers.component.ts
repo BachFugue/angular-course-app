cf3cedde91a7d7a25f293a66c7f13221cd9e5adb
peterlanier <peterlanier@me.com>
2019-05-12 17:02:15 -0600

    Optional ngClass and ngStyle directives challenge

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -11,16 +11,20 @@ export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus = 'No server was created';
   serverName = '';
+  serverCreated = false;
   userName = '';
   userCreationStatus = 'No new users added';
-  
+  servers = ['Test Server', 'Test Server 2'];
+  showDetails = true;
+  detailsLog = [];
+
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
       console.log(this.userName.length);
     }, 2000);
-    
+
   }
 
   ngOnInit() {
@@ -28,6 +32,8 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created. Name is ' + this.serverName;
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
   }
 
   // onUpdateServerName(event: Event) {
@@ -36,6 +42,15 @@ export class ServersComponent implements OnInit {
 
   onCreateUser() {
     this.userCreationStatus = 'New username is ' + this.userName;
+  }
+
+  toggleDetails() {
+    this.detailsLog.push(new Date);
+    return this.showDetails ? this.showDetails = false : this.showDetails = true;
+  }
+
+  getColor(i) {
+    return i >= 4 ? 'dodgerblue' : '';
   }
 
 }
